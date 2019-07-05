aced7edca2eff58e541c4264e1834ad11b23320c
jimmychuang95 <52515320+jimmychuang95@users.noreply.github.com>
2019-07-05 17:59:33 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -2,10 +2,44 @@
 
 */
 
-
+/*
 function start(e) {
+    var name = [
+        {
+            char: '莊',
+            big5: 'B2F8',
+            cns: '1-5C7D',
+            unicode: '838A'
+        },
+        {
+            char: '士',
+            big5: 'A468',
+            cns: '1-4449',
+            unicode: '58EB'
+        },
+        {
+            char: '頡',
+            big5: 'BE65',
+            cns: '1-6F6E',
+            unicode: '9821'
+        },
+    ];
 
+    var nameEl = document.getElementById('div1');
+
+    for (var i = 0; i <= 2; i++) {
+        nameEl.innerHTML += name[i].char;
+        nameEl.innerHTML += ' : big5 = ';
+        nameEl.innerHTML += name[i].big5;
+        nameEl.innerHTML += ' CNS = ';
+        nameEl.innerHTML += name[i].cns;
+        nameEl.innerHTML +=' unicode = ';
+        nameEl.innerHTML += name[i].unicode;
+        nameEl.innerHTML += '<br>';
+    }
 }
+*/
+
 
 /*
 https://www.w3schools.com/jsref/met_document_addeventlistener.asp
@@ -15,4 +49,4 @@ document.addEventListener(event, function, useCapture)
 true - The event handler is executed in the capturing phase
 false- Default. The event handler is executed in the bubbling phase
 */
-window.addEventListener( "load", start, false );
+//window.addEventListener( "load", start, false );
--- portfolio/labs/lab02/lab.js
+++ portfolio/labs/lab02/lab.js
@@ -1,12 +1,33 @@
 /*
 
 */
+function update() {
+    var x = document.getElementById('in1').value;
 
+        var wordsEl = document.getElementById('info2')
 
-function start(e) {
+        var wordUnicode = '';
+
+    for (var i = 0; i < x.length; i++) {
+        wordUnicode += x.charAt(i)
+        wordUnicode += ' U+'
+        wordUnicode += x.charCodeAt(i).toString(16).toUpperCase();
+        wordUnicode += '<br>'
+    }
+
+    wordsEl.innerHTML = wordUnicode;
+
+    document.getElementById("info1").innerHTML = 
+    document.getElementById("in1").value.replace(123, 456)
 
 }
 
+function start(e) {
+        document.getElementById('in1').addEventListener("input", update);
+        update();
+}
+
+window.addEventListener("load", start, false);
 /*
 https://www.w3schools.com/jsref/met_document_addeventlistener.asp
 
@@ -15,4 +36,4 @@ document.addEventListener(event, function, useCapture)
 true - The event handler is executed in the capturing phase
 false- Default. The event handler is executed in the bubbling phase
 */
-window.addEventListener( "load", start, false );
+
